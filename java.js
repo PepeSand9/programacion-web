7c869275421fcd8ea18f97ba16471723ee6c5531
PepeSand9 <franco.n.aguiar@gmail.com>
2023-06-06 15:01:21 -0300

    entrega 3

---
+++ java.js
@@ -0,0 +1,39 @@
+let array1= [1,2,3]
+
+console.log(array1)
+console.log(array[0]);
+
+const ARRAY2 = [1]
+
+
+array1.pop()
+
+
+array1.push(3,14)
+
+
+let a= 0
+
+while(a>1){
+    console.log(a);console.log(b);
+}
+
+let result = '';
+let i = 0;
+
+do {
+  i = i + 1;
+  result = result + i;
+} while (i < 5);
+
+console.log(result);
+
+
+for (let x=0; x<10; x++){
+    console.log(x);
+}
+
+let contar = [0,1,2,3]
+for(let x=0;x>4;x++){
+    console.log(contar[x])
+}
